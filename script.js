872dad072efc939f479e335875fccff9257e965a
diopp <49011476+diopp@users.noreply.github.com>
2022-01-05 00:04:23 -0800

    Modal

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const BASE_URL = 'https://restcountries.com/v3.1/all';
 
 // State Variables
 let apiData;
-
 // Cached Element References
 const $main = $('main');
+//const $form = $('form');
 
 //Event Listeners
-$main.on('mouseover', 'article', handleHover)
+//$form.on('submit', handleSubmit)
 getData()
 //Functions
 function getData() {
@@ -28,15 +28,64 @@ function handleHover() {
     });
 }
 
+function handleSubmit() {
+    console.log('Papa')
+}
 
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime;
 
 function render() {
    const photoFlags =  apiData.map(function(flagObject) {
         return `
-        <article style='background-image: url(${flagObject.flags.svg})'>
-        <p class="hidden">${flagObject.name.common} </p>
+<div class="content"> 
+    <div class="content-overlay">
+</div>
 
-        </article>
+    <article class='content-image' style='background-image: url(${flagObject.flags.svg})'> </article>  
+
+
+        <div class="content-details fadeIn-bottom">
+        <h3 class="content-text"><i class=""></i> ${flagObject.name.common}</h3>
+        <p class="content-text"><i class="bi bi-geo-alt-fill"></i> ${flagObject.region}</p>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+          More Infos
+        </button>
+         
+        <!-- Modal -->
+     
+
+</div>
+</div>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-content">
+    <div class="modal-header bg-success text-center">
+      <h5 class="modal-title text-light " id="exampleModalLongTitle">${flagObject.name.common}</h5>
+    
+    
+    </div>
+    <div class="modal-body">
+      <p> Continent: ${flagObject.region}</p>
+      <p> Capital: ${flagObject.capital} </p>
+      <p> Continent: ${flagObject.region} </p>
+      <p class= 'bi bi-people-fill'>  Population: ${flagObject.population} </p>
+      <p class ='bi bi-geo-fill' > Location: <a href="${flagObject.maps.googleMaps}">link text</a></p>
+      
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+</div>
+</div>
+
+
+       
         
         `
     }).join('');
